cbfec1bdf66b4839691620ee62f4ffb1e565c3ce
Michal Schwarz <michal.schwarz@gmail.com>
2024-01-10 14:59:27 +0100

    fix

--- src/main.cpp
+++ src/main.cpp
@@ -506,30 +506,16 @@ uint32_t read8n(WiFiClient& client, uint8_t *buffer, int32_t bytes)
 }
 
 uint32_t skip(WiFiClient& client, int32_t bytes)
+{
+  return read8n(client, NULL, bytes);
+}
+
 // read one byte safely from WiFiClient, wait a while if data are not available immediately
 uint8_t safe_read(WiFiClient& client)
 {
-  int32_t remain = 1;
-  uint32_t start = millis();
-
   uint8_t ret;
-  while ((client.connected() || client.available()) && (remain > 0))
-  {
-    if (client.available())
-    {
-      int16_t v = client.read();
-      ret = uint8_t(v);
-      remain--;
-    }
-    else delay(1);
-    if (millis() - start > 2000) break; // don't hang forever
-  }
+  read8n(client, &ret, 1);
   return ret;
-}
-
-uint32_t read8n(WiFiClient& client, uint8_t* buffer, int32_t bytes)
-{
-  return read8n(client, NULL, bytes);
 }
 
 uint16_t read16(WiFiClient& client)
