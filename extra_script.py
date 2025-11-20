71c93a084956dd6628996e4ada19a3e8447708c3
Michal Ševčík <multi@tricker.cz>
2025-11-20 20:16:05 +0100

    extra_script.py not used

--- extra_script.py
+++
@@ -1,4 +0,0 @@
-Import("env")
-
-fw_name = (env["PIOENV"]) # get environment name
-env.Replace(PROGNAME="%s_fw_%s" % (fw_name, env.GetProjectOption("custom_fw_version"))) # set custom firmware name & version
