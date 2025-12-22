4a9b18c94a795defc582bab379aa6b95a6421881
Andrej Valek <andy@skyrain.eu>
2025-12-22 16:29:19 +0100

    fix(esp_sleep_enable_ext1_wakeup): replace BIT macro with 1ULL
    While the esp_sleep_enable_ext1_wakeup is using 64-bit mask and BIT is
    producing 32-bit variant, there was a warning about possible wrong
    conversion.

--- src/board.cpp
+++ src/board.cpp
@@ -82,7 +82,7 @@ void enterDeepSleepMode(uint64_t sleepDuration)
   esp_sleep_enable_timer_wakeup(sleepDuration * 1000000ULL);
   // Configure button as additional wake source (wake on LOW level - active low button)
   #if defined(EXT_BUTTON) && !defined(ESPink_V35)
-  esp_sleep_enable_ext1_wakeup(BIT(EXT_BUTTON), ESP_EXT1_WAKEUP_ANY_LOW);
+  esp_sleep_enable_ext1_wakeup(1ULL << EXT_BUTTON, ESP_EXT1_WAKEUP_ANY_LOW);
   #endif
   delay(100);
   esp_deep_sleep_start();
